499fb3323c8d2f8e7b69ece3adc1019e0a815a97
numtel <ben@latenightsketches.com>
2023-10-20 16:49:19 -0700

    Beginning pot share range sliders

--- src/components/MintNew.js
+++ src/components/MintNew.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNetwork, useContractWrite, useWaitForTransaction } from 'wagmi';
 import { useNavigate } from 'react-router-dom';
 import { decodeEventLog } from 'viem';
 import { chainContracts} from '../contracts.js';
 
+const F16 = 0xffffffffffffffff;
+
 export function MintNew() {
   const { chain } = useNetwork();
   const contracts = chainContracts(chain.id);
   const navigate = useNavigate();
+  const [shares, setShares] = useState([[ 0, F16 ]]);
   const submitReply = (event) => {
     event.preventDefault();
     write({
@@ -40,13 +43,47 @@ export function MintNew() {
       navigate(`/lotto/${chain.id}/${contracts.LotteryERC721.address}/${decoded[0].args.tokenId}`);
     },
   });
+  const shareChanged = (index, newValue) => {
+    setShares(shares => {
+      const newShares = [...shares];
+      // TODO adjust other shares to compensate
+      newShares[index][1] = newValue;
+      return newShares;
+    });
+  };
+  const removeShare = (index) => {
+    setShares(shares => {
+      const newShares = [...shares];
+      // Distribute this share's amount to other remaining shares
+      for(let i = 0; i < shares.length; i++) {
+        if(i === index) continue;
+        if(newShares.length === 2) newShares[i][1] = F16;
+        // TODO why not working?
+        else newShares[i][1] = newShares[i][1] * (newShares[index][1] / shares.length - 1);
+      }
+      newShares.splice(index, 1);
+      return newShares;
+    });
+  };
+  // TODO be sure to warn about gas usage of too many shares, calc it!
+  const addShare = (event) => {
+    setShares(shares => {
+      const newShares = [...shares];
+      const newValue = F16 / (shares.length + 1);
+      for(let i = 0; i < shares.length; i++) {
+        newShares[i][1] = newShares[i][1] * ((F16 - newValue) / F16);
+      }
+      newShares.push([0, newValue]);
+      return newShares;
+    });
+  };
   if(!contracts) {
     return (<p>Chain Not Supported!</p>);
   }
   return (
     <form onSubmit={submitReply}>
       <fieldset>
-        <legend>Create New Lottery</legend>
+        <legend>Basic Configuration</legend>
         <div className="field">
           <label>Name:</label>
           <input name="lotteryName" />
@@ -56,6 +93,13 @@ export function MintNew() {
           <textarea name="description"></textarea>
         </div>
         <div className="field">
+          <label>End Time:</label>
+          <input name="endTime" />
+        </div>
+      </fieldset>
+      <fieldset>
+        <legend>Ticket Configuration</legend>
+        <div className="field">
           <label>Token:</label>
           <input name="token" />
         </div>
@@ -63,19 +107,27 @@ export function MintNew() {
           <label>Ticket Price:</label>
           <input name="ticketPrice" />
         </div>
-        <div className="field">
-          <label>End Time:</label>
-          <input name="endTime" />
-        </div>
-        <button type="submit">Submit</button>
-        {isLoading && <p>Waiting for user confirmation...</p>}
-        {isSuccess && (
-          txIsError ? (<p>Transaction error!</p>)
-          : txIsLoading ? (<p>Waiting for transaction...</p>)
-          : txIsSuccess ? (<p>Transaction success!</p>)
-          : (<p>Transaction sent...</p>))}
-        {isError && <p>Error!</p>}
       </fieldset>
+      <fieldset>
+        <legend>Pot Shares Configuration</legend>
+        {shares.map((share, index) => (
+          <div className="share" key={index}>
+            {(share[1] / F16 * 100).toFixed(1)}% to&nbsp;
+            {share[0] === 0 ? 'random winner' : (<span>{share[0]}</span>)}
+            <input name={`share_${index}`} type="range" min="0" max={F16} value={share[1]} onChange={(e) => shareChanged(index, Number(e.target.value))} />
+            {shares.length > 1 && (<button type="button" onClick={(e) => removeShare(index)}>Remove</button>)}
+          </div>
+        ))}
+        <button type="button" onClick={addShare}>Add Share</button>
+      </fieldset>
+      <button type="submit">Submit</button>
+      {isLoading && <p>Waiting for user confirmation...</p>}
+      {isSuccess && (
+        txIsError ? (<p>Transaction error!</p>)
+        : txIsLoading ? (<p>Waiting for transaction...</p>)
+        : txIsSuccess ? (<p>Transaction success!</p>)
+        : (<p>Transaction sent...</p>))}
+      {isError && <p>Error!</p>}
     </form>
   );
 }
